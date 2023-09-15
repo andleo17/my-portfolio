7ce2aad5171fbcba899f9d66addebfbaa551134f
andleo17 <andres.gastulo@gmail.com>
2023-09-14 20:54:46 -0500

    Finishin portfolio

---
+++ src/types.d.ts
@@ -0,0 +1,60 @@
+export interface Information {
+  name: string;
+  lastname: string;
+  email: string;
+  city: string;
+  country: string;
+}
+
+export interface Social {
+  name: string;
+  url: string;
+  icon: string;
+}
+
+export interface Skill {
+  name: string;
+  level: number;
+  icon?: string;
+}
+
+export interface Language {
+  name: string;
+  level: string;
+}
+
+export interface Education {
+  institution: string;
+  start_date: string;
+  end_date?: string;
+  goal: string;
+  country: string;
+  city: string;
+}
+
+export interface Course {
+  name: string;
+  institution: string;
+  start_date: string;
+  end_date?: string;
+  certification_url?: string;
+}
+
+export interface WorkExperience {
+  institution: string;
+  country: string;
+  city: string;
+  start_date: string;
+  end_date?: string;
+  goals: string[];
+  title: string;
+}
+
+export interface Project {
+  name: string;
+  description: string;
+  url?: string;
+  code_url?: string;
+  thumbnails?: string;
+  technologies: string[];
+}
